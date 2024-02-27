bf7d3044184b7af9524f8dca1086899eecfdc027
Stanislaw-P <persaev10@mail.ru>
2024-02-27 12:19:18 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SecondWebApp.Models;
+
+namespace SecondWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Product product = new Product("Коленвал", 15000, "смотри инструкцию", "/images/Мотор.jpg");
+            ViewBag.Product = product;
+            return View();
+        }
+
+        public IActionResult Product(string ProductName, decimal Cost, string Description, string pathImage)
+        {
+            //ViewBag.ProductName = ProductName;
+            //ViewData["ProductName"] = ProductName;
+            ViewBag.Cost = Cost;
+
+            Product product = new Product(ProductName, Cost, Description, pathImage);
+            ViewBag.Product = product;
+
+            //TempData["ProductName"] = ProductName;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecondWebApp.Models
+{
+    public class Product
+    {
+        static int unicId = 0;
+
+        public int Id { get; }
+        public string Name { get; }
+        public Decimal Cost { get; }
+        public string Description { get; }
+        public string PathImage { get; }
+
+        public Product(string name, Decimal cost, string description, string pathImage)
+        {
+            Name = name;
+            Cost = cost;
+            Description = description;
+            PathImage = pathImage;
+            Id = unicId++;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} {Name} {Cost} {Description}";
+        }
+
+    }
+}
